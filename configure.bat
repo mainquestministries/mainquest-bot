f05c9870799ca85fa95e2a6b70db8cac9c279908
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2022-11-13 15:33:41 +0100

    Add remove of init.js

--- configure.bat
+++ configure.bat
@@ -2,4 +2,6 @@ npm install
 npx tsc --target es2017 --module es2022 --moduleResolution node --allowSyntheticDefaultImports scripts/init.ts 
 copy init.js .
 node ./init.js
-exit %ERRORLEVEL%
+set ERROR %ERRORLEVEL%
+rm ./init.js
+exit %ERROR%
--- configure.sh
+++ configure.sh
@@ -3,4 +3,6 @@ npm install
 npx tsc --target es2017 --module es2022 --moduleResolution node --allowSyntheticDefaultImports scripts/init.ts 
 cp scripts/init.js .
 node ./init.js
-exit $?
+export exit_=$?
+rm ./init.js
+exit $exit_
