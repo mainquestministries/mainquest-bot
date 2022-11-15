da8a01f162aa45c8c0e2c666c9a31477505ad2c2
Ronnie1320 <smartpower0105@gmail.com>
2022-11-15 22:18:48 +0100

    Add cleanup.js

---
+++ src/scripts/cleanup.js
@@ -0,0 +1,14 @@
+import { rmSync } from "fs";
+import path, {join} from "path"
+
+const __dirname = path.resolve(path.dirname(''))
+
+rmSync(join(__dirname, "./node_modules"), {
+    force: true,
+    recursive: true
+})
+
+rmSync(join(__dirname, "./dist"), {
+    force: true,
+    recursive: true
+})
