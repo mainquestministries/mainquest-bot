57a99ae86a41593a52ccabd692d02180c5e85649
Ronnie1320 <smartpower0105@gmail.com>
2022-11-17 14:35:43 +0100

    Add no migration on dedicated db

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -1,2 +1,8 @@
-npx prisma migrate deploy
+if env | grep -q DATABASE_URL=
+then
+    echo Using dedicated Database...
+    echo Migrations are not automatically applied!
+else 
+    npx prisma migrate deploy
+fi
 npm run start
