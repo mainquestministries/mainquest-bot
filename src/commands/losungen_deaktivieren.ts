14a318894b5028e4288b55b6507dc3feca03564a
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2022-11-30 17:47:05 +0100

    add losungs_commands

---
+++ src/commands/losungen_deaktivieren.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+const prisma = new PrismaClient()
+@ApplyOptions<Command.Options>({
+	description: 'Losungschannel deaktivieren'
+})
+export class UserCommand extends Command {
+	public constructor(context: Command.Context) {
+		super(context, {
+			preconditions: ['admin', 'guildchannel'],
+			cooldownDelay: 10_000
+		});
+	}
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand({
+			name: this.name,
+			description: this.description
+		})
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		await prisma.losungen.deleteMany( {
+			where: {
+				id: interaction.guildId as string
+			}
+		})
+		return interaction.reply({
+			embeds: [
+				{
+					color: 0x12d900,
+					title: 'Konfiguration gespeichert',
+					description: 'Losungschannel deaktiviert.'
+				}
+			],
+			ephemeral: true
+		});
+	}
+}
---
+++ src/commands/losungschannel.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+const prisma = new PrismaClient()
+@ApplyOptions<Command.Options>({
+	description: 'Losungschannel hier platzieren.'
+})
+export class UserCommand extends Command {
+	public constructor(context: Command.Context) {
+		super(context, {
+			preconditions: ['admin', 'guildchannel'],
+			cooldownDelay: 10_000
+		});
+	}
+
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand({
+			name: this.name,
+			description: this.description
+		})
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		try {
+			await prisma.losungen.findFirstOrThrow({
+				where: {
+					id: interaction.guildId as string
+				}
+			});
+
+			await prisma.losungen.update({
+				where: {
+					id: interaction.guildId as string
+				},
+				data: {
+					channelId: interaction.channelId
+				}
+			});
+		} catch (e) {
+			await prisma.losungen.create({
+				data: {
+					id: interaction.guildId as string,
+					channelId: interaction.channelId
+				}
+			});
+		}
+		return interaction.reply({
+			embeds: [
+				{
+					color: 0x12d900,
+					title: 'Konfiguration gespeichert',
+					description: 'Losungschannel aktualisiert.'
+				}
+			],
+			ephemeral: true
+		});
+}}
