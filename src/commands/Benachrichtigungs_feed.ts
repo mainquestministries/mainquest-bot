fca3951e6611ed0a3fed1f5c6d5caa4106d2dca4
Ronnie1320 <smartpower0105@gmail.com>
2023-03-07 20:09:31 +0100

    make benachrichtigungs_feed work

--- src/commands/Benachrichtigungs_feed.ts
+++ src/commands/Benachrichtigungs_feed.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { EmbedBuilder } from 'discord.js';
 const prisma = new PrismaClient();
 @ApplyOptions<Command.Options>({
 	description: 'Erhalte deine abonnierten Gebetsanliegen sofort'
@@ -15,6 +16,57 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		return interaction.reply({ content: 'Hello world!' });
+		try {
+			const msg = await prisma.message.findFirstOrThrow({
+				where: {
+					id: interaction.user.id
+				}, include: {
+					embeds: true
+				}
+			})
+			if (msg.embeds.length==0) {
+				throw Error;
+			}
+			let embeds: EmbedBuilder[] = [];
+				msg.embeds.forEach(async (embed_) => {
+					const embed = await prisma.embed.findUniqueOrThrow({
+						where: {
+							id: embed_.id
+						},
+						include: {
+							Swallowed: true
+						}
+					});
+					let color_temp = 0;
+					if (embed.Swallowed.color === null) {
+						color_temp = 0;
+					} else {
+						color_temp = embed.Swallowed.color;
+					}
+					const temp_embed = new EmbedBuilder()
+						.setTitle(`Gebetsanliegen von ${embed.Swallowed.author}`)
+						.setDescription(embed.Swallowed.message_content)
+						.setColor(color_temp)
+						.setAuthor({
+							name: embed.Swallowed.author,
+							iconURL: embed.Swallowed.author_avatar_url ?? undefined
+						})
+						
+					embeds.push(temp_embed);
+				});
+			return interaction.reply({ content: 'Dein Feed', embeds: embeds, ephemeral: true })
+		} catch {
+			return interaction.reply({
+				embeds: [
+					{
+						color: 0xff0000,
+						title: 'Befehl verweigert',
+						description: 'Du hast anscheinend nichts abonniert.'
+					}
+				],
+				ephemeral: true
+			})
+		}
+		;
 	}
 }
