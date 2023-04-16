7ba473c2a4af81b317f2399107c09b4ef7de4d92
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-04-16 16:00:09 +0200

    Revert "Merge branch 'master' of https://github.com/mainquestministries/mainquest-bot"
    This reverts commit 3846c0b07d76f632385673e2c254ef0e82b7cb41, reversing
    changes made to c346527ab636869bda3e962f9da4d4a4aafdb02e.

--- build.nix
+++ build.nix
@@ -1,6 +1,8 @@
-{ pkgs ? import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
-}) { } }:
+{
+  pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
+  }) {}
+}:
 let
   pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
@@ -12,37 +14,40 @@ let
   # Sandbox workaround: https://www.nmattia.com/posts/2022-12-18-lockfile-trick-package-npm-project-with-nix/
 
   # Read the package-lock.json as a Nix attrset
-  packageLock =
-    builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
+  packageLock = builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
 
-in pkgs.stdenv.mkDerivation rec {
+in
+
+pkgs.stdenv.mkDerivation rec {
   inherit (packageLock) name version;
   pname = name;
   inherit src;
 
+
   buildInputs = [
     pkgs.nodejs-18_x
-    pkgs.nodePackages.node-gyp-build
+    pkgs.nodePackages.node-gyp-build 
     pkgs.gcc9
-    pkgs.python310Full
+    pkgs.python310Full 
     pkgs.gnumake
     pkgs.python310Packages.gyp
   ];
 
   buildPhase = ''
-    export HOME=$PWD/.home
-    export npm_config_cache=$PWD/.npm
-    npm ci --no-progress
-    npx prisma generate
-    npm run build:production
-    rm -f dist/.tsbuildinfo
+     export HOME=$PWD/.home
+     export npm_config_cache=$PWD/.npm
+     npm ci --no-progress
+     npx prisma generate
+     npm run build:production
+     python3 scripts/version.py >> version.txt
+     rm -f dist/.tsbuildinfo
   '';
 
   installPhase = ''
     mkdir -p $out
     cp -t $out -r dist/ -r prisma/ LICENSE package-lock.json package.json
     cd $out
-    npm ci --omit=dev --no-progress
+    npm ci --omit=dev
     npx prisma generate
   '';
 
