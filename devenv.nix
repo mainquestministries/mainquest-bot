07713d4aec4e39616d40a149673a50967adb5678
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-04-16 16:03:18 +0200

    Revert "Remove hooks"
    This reverts commit c346527ab636869bda3e962f9da4d4a4aafdb02e.

--- devenv.nix
+++ devenv.nix
@@ -3,17 +3,16 @@
 {
   # https://devenv.sh/basics/
   env.GREET = "mainquestbot-devenv";
+  
 
   # https://devenv.sh/packages/
-  packages = [
-    pkgs.git
-    pkgs.nodejs-18_x
-    pkgs.nodePackages.node-gyp-build
-    pkgs.gcc9
-    pkgs.python310Full
-    pkgs.gnumake
-    pkgs.python310Packages.gyp
-  ];
+  packages = [  pkgs.git 
+                pkgs.nodejs-18_x 
+                pkgs.nodePackages.node-gyp-build 
+                pkgs.gcc9
+                pkgs.python310Full 
+                pkgs.gnumake
+                pkgs.python310Packages.gyp];
 
   # https://devenv.sh/scripts/
   scripts.migrate.exec = "npx prisma migrate dev";
@@ -27,13 +26,17 @@
   services.mysql.enable = true;
   services.mysql.package = pkgs.mariadb;
   services.mysql.initialDatabases = [{ name = "discord"; }];
-  services.mysql.ensureUsers = [{
-    name = "bot";
-    password = "12345";
-    ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
-  }];
+  services.mysql.ensureUsers = [
+    {
+      name = "bot";
+      password = "12345";
+      ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
+    }
+  ];
 
-  processes = { node.exec = "npm run watch:start"; };
+  processes = {
+    node.exec= "npm run watch:start";
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
